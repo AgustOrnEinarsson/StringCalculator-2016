3ffe1bdaecd17509f0975940c5485100b5577ec3
AgustOrnEinarsson <gustieinars@gmail.com>
2016-10-11 12:03:17 -0700

    Test two numbers passed

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,7 +5,10 @@ public class Calculator{
 	public static int add(String s){
 		if (s.equals(""))
 			return 0;
-			
+		else if(s.contains(","){
+			String [] numbers = s.split(",");
+			return Integer.toInt(numbers[0]) + Integer.toInt(numbers[1])
+		}
 		return 5;
 	}
 }
