9e41dcd617645e3e53ced115ab1ce764d4be2200
AgustOrnEinarsson <gustieinars@gmail.com>
2016-10-11 11:50:42 -0700

    Test one argument failes

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -9,4 +9,9 @@ public class CalculatorTest{
 	public void testEmptyString(){
 		assertEquals(0, Calculator.add(""));
 	}
+
+	@Test
+	public void testOneNumber(){
+		assertEquals(5, Calculator.add("5"));
+	}
 }
